6ade90adc759ae63c64c09a02afb61c924cb3363
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-05-24 15:03:28 +0100

    Voters

--- administrator/urls.py
+++ administrator/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name="adminDashboard"),
+    path('voters', views.voters, name="adminViewVoters"),
     # path('register/', views.account_register, name="account_register"),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def dashboard(request):
     context = {}
     return render(request, "admin/home.html", context)
+
+
+def voters(request):
+    context = {}
+    return render(request, "admin/voters.html", context)
