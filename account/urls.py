7a4463a07c77718a012f461c7269d73c3f922217
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-05-24 14:43:13 +0100

    Admin Dashboard

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path('login/', views.account_login, name="account_login"),
     path('register/', views.account_register, name="account_register"),
+    path('logout/', views.account_logout, name="account_logout"),
 ]
