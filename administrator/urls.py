7702ab292d6deb005510cb7e4e89fbd11709e2ef
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-05-26 01:43:51 +0100

    Ballot Position

--- administrator/urls.py
+++ administrator/urls.py
@@ -9,15 +9,23 @@ urlpatterns = [
     path('voters/view', views.view_voter_by_id, name="viewVoter"),
     path('voters/delete', views.deleteVoter, name='deleteVoter'),
     path('voters/update', views.updateVoter, name="updateVoter"),
+
     # * Position
     path('position/view', views.view_position_by_id, name="viewPosition"),
     path('position/update', views.updatePosition, name="updatePosition"),
     path('position/delete', views.deletePosition, name='deletePosition'),
     path('positions/view', views.viewPositions, name='viewPositions'),
+
     # * Candidate
     path('candidate/', views.viewCandidates, name='viewCandidates'),
     path('candidate/update', views.updateCandidate, name="updateCandidate"),
     path('candidate/delete', views.deleteCandidate, name='deleteCandidate'),
     path('candidate/view', views.view_candidate_by_id, name='viewCandidate'),
+
+    # * Settings (Ballot Position and Election Title)
+    path("settings/ballot/position", views.ballot_position, name='ballot_position'),
+    path("settings/ballot/position/update/<int:position_id>/<str:up_or_down>/",
+         views.update_ballot_position, name='update_ballot_position'),
+
 
 ]
