8aa84dc13b9ebd0bf1720867cdcaf554e88c524f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-06-02 11:16:46 +0100

    Modified DB

--- voting/models.py
+++ voting/models.py
@@ -7,7 +7,7 @@ class Voter(models.Model):
     admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     phone = models.CharField(max_length=11, unique=True)  # Used for OTP
     otp = models.CharField(max_length=10, null=True)
-    verified = models.IntegerField(default=1)
+    verified = models.IntegerField(default=0)  # 0 stabd
     voted = models.IntegerField(default=0)  # 0 means voter has not voted
     # Will later limit how many OTPs are sent
 
