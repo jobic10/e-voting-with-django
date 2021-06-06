0cbdedc48c0da3b950f1a8789b481749c68716f0
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-06-06 20:53:33 +0100

    Middleware fully setup

--- account/middleware.py
+++ account/middleware.py
@@ -1,6 +1,7 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.urls import reverse
 from django.shortcuts import redirect
+from django.contrib import messages
 
 
 class AccountCheckMiddleWare(MiddlewareMixin):
@@ -9,11 +10,29 @@ class AccountCheckMiddleWare(MiddlewareMixin):
         user = request.user  # Who is the current user ?
         if user.is_authenticated:
             if user.user_type == '1':  # Admin
-                if modulename == 'main_app.student_views':
-                    return redirect(reverse('admin_home'))
+                if modulename == 'voting.views':
+                    error = True
+                    from urllib.parse import urlparse
+                    url = urlparse(request.path).path
+                    from django.urls import resolve
+                    try:
+                        redirect_url = resolve(url)
+                        if redirect_url.url_name == 'fetch_ballot':
+                            error = False
+                            pass
+                    except Exception as e:
+                        pass
+                    if error:
+                        messages.error(
+                            request, "You do not have access to this resource")
+                        return redirect(reverse('adminDashboard'))
+
             elif user.user_type == '2':  # Voter
-                if modulename == 'main_app.student_views' or modulename == 'main_app.hod_views':
-                    return redirect(reverse('staff_home'))
+                # or modulename == 'main_app.hod_views':
+                if modulename == 'administrator.views':
+                    messages.error(
+                        request, "You do not have access to this resource")
+                    return redirect(reverse('voterDashboard'))
             else:  # None of the aforementioned ? Please take the user to login page
                 return redirect(reverse('account_login'))
         else:
