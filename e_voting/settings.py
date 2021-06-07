6f7c15477a0b70234135b1578a5703ca45f5d782
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-06-07 15:11:40 +0100

    Final Release

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = 'e_voting.wsgi.application'
 
 DATABASES = {
     #   You can use this :
-    #   'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
-    #     }
-
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'e_voting',
-        'HOST': '127.0.0.1',
-        'USER': 'root',
-        'PASSWORD': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'e_voting',
+    #     'HOST': '127.0.0.1',
+    #     'USER': 'root',
+    #     'PASSWORD': ''
+    # }
 }
 
 
